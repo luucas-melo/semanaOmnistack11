d6fae06cd6f97d0a0dc891e3e72f1bf71bbd1d4b
luucasxd <lucasmelodos322@gmail.com>
2020-07-17 20:50:52 -0300

    reenviando frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './global.css'
+//JSX (javascript e XML)
+
+import Routes from './routes';
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
